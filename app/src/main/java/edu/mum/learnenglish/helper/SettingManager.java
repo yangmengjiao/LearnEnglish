24184a555abf75f76e1a2899bfd563988f625138
yangmengjiao <mengjiaoshun@gmail.com>
2018-04-29 15:24:50 -0500

    add setting play time function

--- app/src/main/java/edu/mum/learnenglish/helper/SettingManager.java
+++ app/src/main/java/edu/mum/learnenglish/helper/SettingManager.java
@@ -11,6 +11,8 @@ public class SettingManager {
     private static final String KEY_ENGLISH_ONLY_FLAG = "englishOnly";
     private static final int defaultEnglishOnlyValue = 0;
 
+    private static final String KEY_PLAY_TIME = "playTime";
+    private static final String defaultKEYPLAYTIME = "forever";//for ever loop
 
     public static float getLightSensorFlag(Context context) {
 
@@ -28,6 +30,15 @@ public class SettingManager {
 
     public static void setEnglishOnlyFlag(Context context,int value) {
         PrefUtils.setInt(context, KEY_ENGLISH_ONLY_FLAG, value);
+    }
+
+    public static String getPlayTime(Context context) {
+
+        return PrefUtils.getString(context, KEY_PLAY_TIME, defaultKEYPLAYTIME);
+    }
+
+    public static void setPlayTime(Context context,String value) {
+        PrefUtils.setString(context, KEY_PLAY_TIME, value);
     }
 
 
